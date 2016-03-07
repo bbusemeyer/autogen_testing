4a3f135fe0ffcb067237cd1b5ea0866cb923f007
Brian Busemeyer <bbusemeyer@gmail.com>
2016-03-06 21:43:58 -0600

    Simplify tests for new format.

--- run_tests.py
+++ run_tests.py
@@ -20,18 +20,18 @@ element_list.append(runqwalk.Crystal2QWalk())
 element_list.append(runqwalk.QWalkVarianceOptimize(
   submitter=veritas.LocalVeritasQwalkSubmitter(
     nn=1,np=8,time="0:10:00",queue="batch")))
-element_list.append(runqwalk.QWalkEnergyOptimize(
-  submitter=veritas.LocalVeritasQwalkSubmitter(
-    nn=1,np=8,time="0:10:00",queue="batch")))
-element_list.append(runqwalk.QWalkRunVMC(
-  submitter=veritas.LocalVeritasQwalkSubmitter(
-    nn=1,np=8,time="0:10:00",queue="batch")))
+#element_list.append(runqwalk.QWalkEnergyOptimize(
+#  submitter=veritas.LocalVeritasQwalkSubmitter(
+#    nn=1,np=8,time="0:10:00",queue="batch")))
+#element_list.append(runqwalk.QWalkRunVMC(
+#  submitter=veritas.LocalVeritasQwalkSubmitter(
+#    nn=1,np=8,time="0:10:00",queue="batch")))
 element_list.append(runqwalk.QWalkRunDMC(
   submitter=veritas.LocalVeritasQwalkSubmitter(
     nn=1,np=8,time="0:20:00",queue="batch")))
-element_list.append(runqwalk.QWalkRunPostProcess(
-  submitter=veritas.LocalVeritasQwalkSubmitter(
-    nn=1,np=8,time="0:20:00",queue="batch")))
+#element_list.append(runqwalk.QWalkRunPostProcess(
+#  submitter=veritas.LocalVeritasQwalkSubmitter(
+#    nn=1,np=8,time="0:20:00",queue="batch")))
 
 default_job=jc.default_job_record("si.cif")
 default_job['dft']['kmesh'] = [2,2,2]
